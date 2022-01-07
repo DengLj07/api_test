9b9f55a245fe0bc8a00bf9dec1b903f6e5d60cf7
somethingimportant <48822791+somethingimportant@users.noreply.github.com>
2022-01-07 20:25:50 +0800

    Add files via upload
    add

---
+++ MyApiProject/base/environment.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/12/24
+@Auth ： wb.denglijiao@mesg.corp.netease.com
+@Dev : 
+@Doc Url :
+"""
+
+
+class Url:
+
+    def __init__(self, host: str, port: int, schema: str = 'http'):
+        self.host = host
+        self.port = port
+        self.schema = schema
+
+    def __repr__(self):
+        return f'{self.host}://{self.host}:{self.port}'
+
+
+class Environment:
+    """环境"""
+
+    @property
+    def manager_url(self) -> Url:
+        raise NotImplementedError
+
+    @property
+    def business_url(self) -> Url:
+        raise NotImplementedError
+
+    @property
+    def staging_env(self) -> Url:
+        raise NotImplementedError
+
+    @property
+    def product_env(self) -> Url:
+        raise NotImplementedError
+
+
+class Test01Environment(Environment):
+    """测试服01"""
+
+    @property
+    def manager_url(self):
+        return Url('test-operation.netease.com', 80)
+    # 管理后台地址
+
+
+class Test03Environment(Environment):
+    """测试服03"""
+
+    @property
+    def manager_url(self):
+        return Url('test-operation.netease.com', 80)
+
+
+class StagingEnvironment(Environment):
+    """预发服"""
+
+    @property
+    def manager_url(self):
+        return Url('staging-operation.netese.com', 80)
+
+
+def make_env(env_str: str = 'test01') -> Environment:
+
+    if env_str == 'test01':
+        return Test01Environment()
+    if env_str == 'test03':
+        return Test03Environment()
+    if env_str == 'staging':
+        return StagingEnvironment()
+
+    raise Exception(f'env_str error:{env_str}')
+
